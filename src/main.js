58077095f0a6f27a1ec843932ae23a7209908204
D068017 <marcel.dambach@sap.com>
2020-07-27 12:13:29 +0200

    Remove file reading step (use it as warm-up  exercise)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const fs = require('fs');
 const Pool = require('pg').Pool;
 const URI = 'postgres://postgres@localhost:5432/postgres';
 const Paymentgateway = require('./payment-gateway');
@@ -9,57 +8,35 @@ const paymentGateway = new Paymentgateway([
     id: 123,
     name: 'John Doe',
     balance: 2500
-  },
-  {
-    id: 456,
-    name: 'Jane Doe',
-    balance: 300
   }
 ]);
 
 const pool = new Pool({connectionString: URI});
 
 const chargeUser = (userId, amount) => {
-  fs.readFile('src/file.csv', 'utf-8', (err, data) => {
+  pool.connect((err, client) => {
     if (err) {
-      throw new Error('Failed to read file');
-    }
-    const users = data.split('\n').slice(1).map((line) => {
-      line = line.split(';');
-      return {
-        id: parseInt(line[0]),
-        amount: parseInt(line[1])
-      };
-    });
-
-    const [user] = users.filter((user) => user.id === userId)
-    if (!user) {
-      throw new Error('Specified user has no file entry.');
+      console.error(err);
+      throw new Error('Failed to connect to Databse');
     }
 
-    pool.connect((err, client) => {
+    client.query('SELECT * FROM users WHERE id = $1', [userId], (err, result) => {
       if (err) {
-        console.error(err);
-        throw new Error('Failed to connect to Databse');
+        throw new Error('Failed to query user');
       }
 
-      client.query('SELECT * FROM users WHERE id = $1', [user.id], (err, result) => {
+      const dbEntry = result.rows[0];
+      paymentGateway.charge(dbEntry.gateway_id, amount, (err, response) => {
         if (err) {
-          throw new Error('Failed to query user');
+          throw err;
         }
-        const dbEntry = result.rows[0];
-        paymentGateway.charge(dbEntry.gateway_id, amount, (err, response) => {
-          if (err) {
-            throw err;
-          }
-          else {
-            console.log('Charged successfully')
-          }
-        });
+        else {
+          console.log('Charged successfully')
+        }
       });
     });
   });
-}
+};
 
 try {
   chargeUser(1, 500);
