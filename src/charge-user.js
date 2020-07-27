39f4640da334a0dbead1b48d0a2c88da25b7e831
D068017 <marcel.dambach@sap.com>
2020-07-27 12:44:25 +0200

    Extract the charge user function

---
+++ src/charge-user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const chargeUser = (userId, amount, paymentGateway, pool, callback) => {
+  pool.connect((err, client) => {
+    if (err) {
+      return callback(new Error('Failed to connect to database'));
+    }
+
+    client.query('SELECT * FROM users WHERE id = $1', [userId], (err, result) => {
+      if (err) {
+        return callback(new Error('Query failed'));
+      }
+
+      if (!result.rows[0]) {
+        return callback(new Error('No user found in database'));
+      }
+      const dbEntry = result.rows[0];
+      paymentGateway.charge(dbEntry.gateway_id, amount, (err, response) => {
+        if (err) {
+          return callback(err);
+        }
+        else {
+          return callback(null, response);
+        }
+      });
+    });
+  });
+};
+
+module.exports = chargeUser;
--- src/main.js
+++ src/main.js
@@ -3,43 +3,24 @@
 const Pool = require('pg').Pool;
 const URI = 'postgres://postgres@localhost:5432/postgres';
 const Paymentgateway = require('./payment-gateway');
-const paymentGateway = new Paymentgateway([
-  {
-    id: 123,
-    name: 'John Doe',
-    balance: 2500
-  }
-]);
-
 const pool = new Pool({connectionString: URI});
-
-const chargeUser = (userId, amount) => {
-  pool.connect((err, client) => {
-    if (err) {
-      console.error(err);
-      throw new Error('Failed to connect to Databse');
-    }
-
-    client.query('SELECT * FROM users WHERE id = $1', [userId], (err, result) => {
-      if (err) {
-        throw new Error('Failed to query user');
-      }
-
-      const dbEntry = result.rows[0];
-      paymentGateway.charge(dbEntry.gateway_id, amount, (err, response) => {
-        if (err) {
-          throw err;
-        }
-        else {
-          console.log('Charged successfully')
-        }
-      });
-    });
-  });
-};
+const chargeUser = require('./charge-user');
 
 try {
-  chargeUser(1, 500);
+  const paymentGateway = new Paymentgateway([
+    {
+      id: 123,
+      name: 'John Doe',
+      balance: 2500
+    }
+  ]);
+  chargeUser(1, 500, paymentGateway, pool, (err, response) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Successfully charged user');
+    }
+  });
 } catch (err) {
   console.error(err);
 }
