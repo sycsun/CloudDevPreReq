d1253a8d5699069027485c2312c16a7514daff44
Jens Keller <jens.keller@sap.com>
2020-08-30 16:11:43 +0200

    improvements to file read scenario

--- src/file-server.js
+++ src/file-server.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const express = require('express');
 
-fs.writeFileSync('large-file.txt', 'Because I\'m happy. '.repeat(1000000));        
+fs.writeFileSync('large-file.txt', 'Because I\'m happy. '.repeat(1000000));
 
 const app = express();
 
--- test/file-server.test.js
+++ test/file-server.test.js
@@ -3,13 +3,14 @@ const supertest = require('supertest');
 const app = require('../src/file-server');
 
 describe('File server', () => {
+  
   it('can efficiently handle parallel requests', () => {
     const client = supertest(app);
     const startTime = Date.now();
 
     const requests = [];
     for (let i = 0; i < 10; i++) {
-        requests.push(client.get('/large-file.txt'));
+        requests.push(client.get('/large-file.txt').expect(200));
     }
 
     return Promise.all(requests).then(() => {
